8a40fee5a8bcf58f07a810e64a793386d0ba3212
Thaparoshan143 <101375593+Thaparoshan143@users.noreply.github.com>
2026-05-03 19:55:09 +0545

    fix: convert to .ts file (prev txt mistake)

---
+++ app/robots.ts
@@ -0,0 +1,12 @@
+import { MetadataRoute } from 'next';
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: '/private/',
+    },
+    sitemap: 'https://jigustore.com.np/sitemap.xml',
+  };
+}
